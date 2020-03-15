c56686836f70126b7c88b6b55c899ea50ca48167
AJ Rasmussen <aj.rasmussen@outlook.com>
2020-03-14 21:46:41 -0700

    First commit - rpbi installs

---
+++ env_setup/rbpi_installs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import apt
+import sys
+
+PKG_LIST = [
+    "vim",
+    "git",
+    "default-jdk",
+    "python2.7",
+    "black",
+    "ant",
+    "libssl-dev",
+    "autoconf",
+    "automake",
+    "libtool",
+]
+
+
+def install(cache, pkg_name):
+    pkg = cache[pkg_name]
+    if pkg.is_installed:
+        print(f"{pkg_name} is already installed. ")
+        return
+    else:
+        pkg.mark_install()
+    try:
+        cache.commit()
+    except Exception as e:
+        print(sys.stderr, f"Unable to install package. {e}")
+
+
+def main(pkg_list):
+    cache = apt.cache.Cache()
+    cache.update()
+    cache.open()
+
+    for i in pkg_list:
+        install(cache, i)
+
+
+if __name__ == "__main__":
+    sys.exit(main(PKG_LIST))
