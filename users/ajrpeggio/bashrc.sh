b24790bd22a6768eed45ab427c841ca1f804fa1b
ajrpeggio <aj.rasmussen@outlook.com>
2020-03-25 19:27:55 -0700

    Updating .bashrc and shebang for python files

--- users/ajrpeggio/bashrc.sh
+++ users/ajrpeggio/bashrc.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
 # Default Editor
 export EDITOR=vim
+export HISTSIZE=50000
 
 # Source global definitions
-if [ -f /etc/bashrc ]; then
-    . /etc/bashrc
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
 fi
+
+# User Specific Aliases
+alias rascode="cd ${HOME}/rascode/"
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
 
 # User Specific Functions
 bo ()
@@ -17,14 +29,7 @@ bi ()
     cd $(echo $PWD | cut -d '/' -f 1-6,9-)
 }
 
-# User Specific Aliases
-
-alias rascode="cd ${HOME}/rascode/"
-
 # _scm_prompt
-
-export HISTSIZE=50000
-
 RED_0=$(    tput setaf 1)
 GREEN_0=$(  tput setaf 2)
 RED="\[$RED_0\]"
@@ -46,3 +51,23 @@ _prompt_command() {
 }
 
 export PROMPT_COMMAND="_prompt_command"
+
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
