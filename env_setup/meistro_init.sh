c0efd50f779b0164880316a85555a6782cead430
ajrpeggio <aj.rasmussen@outlook.com>
2020-04-07 04:12:25 +0000

    Some env setup consolidation

---
+++ env_setup/meistro_init.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+function aptInstalls() {
+   arr=("$@")
+   for i in ${arr[@]};
+      do
+          sudo apt install "${i}" -y
+      done
+
+}
+
+INIT_PKGS=(
+    "curl"
+    "mercurial"
+    "net-tools"
+    "openssh-server"
+    "git"
+    "vim"
+    "default-jdk"
+    "python3-pip"
+)
+
+aptInstalls ${INIT_PKGS[@]}
+
+###
+###
+
+WM_PKGS=(
+    "build-essential"
+    "libssl-dev"
+    "autoconf"
+    "automake"
+    "libtool"
+    "python-dev"
+    "python-setuptools"
+    "python3-setuptools"
+)
+
+aptInstalls ${WM_PKGS[@]}
+
+
+git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
+cd watchman
+./autogen.sh
+./configure --with-python=/usr/bin/python3 --enable-conffile=/etc/watchman.json
+make
+sudo make install
+
+###
+###
+
+BUCK_PKGS=(
+    "openjdk-8-jre"
+    "ant"
+    "python2.7"
+    "git"
+)
+
+aptInstalls ${BUCK_PKGS[@]}
+
+
+git clone https://github.com/facebook/buck.git
+cd buck
+ant
+./bin/buck build --show-output buck
+buck-out/gen/ce9b6f2e/programs/buck.pex --help
+
+sudo ln -s ${PWD}/bin/buck /usr/bin/buck
+sudo ln -s ${PWD}/bin/buckd /usr/bin/buckd
+
+
