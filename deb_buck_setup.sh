a4080adabc31ca1e70efce43edc1e4d5a262eff7
ajrpeggio <aj.rasmussen@outlook.com>
2020-09-19 00:20:13 +0000

    Updating dep_buck_setup.sh and moving third party modules

--- deb_buck_setup.sh
+++ deb_buck_setup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-function aptInstalls() {
-   arr=("$@")
-   for i in ${arr[@]};
-      do
-          sudo apt install "${i}" -y
-      done
 
-}
-
-WM_PKGS=(
+DEB_PKGS=(
+    "python3"
+    "vim"
     "build-essential"
     "libssl-dev"
     "autoconf"
@@ -20,42 +14,59 @@ WM_PKGS=(
     "python3-setuptools"
 )
 
-aptInstalls ${WM_PKGS[@]}
-
-
-git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
-cd watchman
-./autogen.sh
-./configure --with-python=/usr/bin/python3 --enable-conffile=/etc/watchman.json
-make
-sudo make install
-
-##
-##
-
-cd ${HOME}
-
-
-###
-###
 
 BUCK_PKGS=(
+    "watchman"
     "openjdk-8-jre"
     "ant"
     "python2.7"
     "git"
 )
 
+
+function aptInstalls() {
+   arr=("$@")
+   for i in ${arr[@]};
+      do
+          sudo apt install "${i}" -y
+      done
+
+} 
+
+
+function wmInstall() {
+    cd ${HOME}
+    git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
+    cd watchman/
+    ./autogen.sh
+    ./configure --with-python=/usr/bin/python3 --enable-conffile=/etc/watchman.json
+    make
+    sudo make install
+}
+
+
+function buckInstall() {
+    cd ${HOME}
+    /usr/bin/git clone https://github.com/facebook/buck.git 
+    cd buck
+    ant
+    ./bin/buck build --show-output buck
+    buck-out/gen/ce9b6f2e/programs/buck.pex --help
+    
+    # Creating symlink for buck & buckd under /usr/bin
+    sudo ln -s ${PWD}/bin/buck /usr/local/bin/buck
+    sudo ln -s ${PWD}/bin/buckd /usr/local/bin/buckd
+}
+
+
+# Installing deps
+aptInstalls ${DEB_PKGS[@]}
 aptInstalls ${BUCK_PKGS[@]}
 
+sudo apt update
+sudo apt upgrade -y
+# Calling Watchman Install Function
+# wmInstall
 
-git clone https://github.com/facebook/buck.git
-cd buck
-ant
-./bin/buck build --show-output buck
-buck-out/gen/ce9b6f2e/programs/buck.pex --help
-
-sudo ln -s ${PWD}/bin/buck /usr/bin/buck
-sudo ln -s ${PWD}/bin/buckd /usr/bin/buckd
-
-
+# Calling Buck Install Function
+buckInstall
